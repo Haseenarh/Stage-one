7082d8a634220df088e1da3efb6ef2044a11c967
Haseenarh <hassanatbello93@gmail.com>
2023-09-08 23:36:30 +0100

    mini changes

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// Get the current day of the week
+
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDay = days[new Date().getDay()];
 
-// Check if the current day is Friday
+
 if (currentDay === "Friday") {
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
 } else {
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = "Today is not Friday. It's " + currentDay;
 }
 
-// Create a Date object with the provided time "11:32:37"
-// Assuming the date is today's date
+
 const currentDate = new Date();
 const timeString = "11:32:37";
 const timeParts = timeString.split(":");
 const customDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), timeParts[0], timeParts[1], timeParts[2]);
 
-// Get the UTC time in milliseconds
+
 const utcMilliseconds = customDate.getTime();
 document.querySelector('[data-testid="currentUTCTime"]').textContent = utcMilliseconds;
 
